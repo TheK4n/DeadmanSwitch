16bfb28f8852c6e297c3de2bf49a87310dc26860
TheK4n <djvlad967891@gmail.com>
2022-07-03 20:53:32 +0300

    hash

--- server.go
+++ server.go
@@ -5,13 +5,20 @@ import (
     // "flag"
     "log"
     "fmt"
+    "crypto/sha256"
+    "encoding/hex"
+    "syscall"
 )
 
 
 func main() {
 
+    socketPath := "/tmp/deadman.socket"
+
+
     log.Printf("server starts")
-    listener, _ := net.Listen("unix", "/tmp/deadman.socket")
+    syscall.Unlink(socketPath)
+    listener, _ := net.Listen("unix", socketPath)
 
     for {
         conn, err := listener.Accept()
@@ -21,6 +28,12 @@ func main() {
 
         go HandleClient(conn)
     }
+}
+
+func hashPassphrase(passphrase string) string {
+    h := sha256.New()
+    h.Write([]byte(passphrase))
+    return hex.EncodeToString(h.Sum(nil))
 }
 
 func HandleClient(conn net.Conn) {
@@ -37,7 +50,7 @@ func HandleClient(conn net.Conn) {
             break
         }
 
-        if (string(buf[:readLen]) == "password") {
+        if hashPassphrase(string(buf[:readLen])) == "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8" {
             conn.Write([]byte("Accepted")) // пишем в сокет
         } else {
             conn.Write([]byte("Declined")) // пишем в сокет
